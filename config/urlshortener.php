c37f88bac84f6cd949aafc9b160043a2e22a3689
Michalis Antoniou <MAntoniou@CircleLinkHealth.com>
2018-01-09 22:08:02 +0200

    Adds generic access token driver

--- config/urlshortener.php
+++ config/urlshortener.php
@@ -9,6 +9,9 @@ return [
         'username' => env('URL_SHORTENER_BITLY_USERNAME', ''),
         'password' => env('URL_SHORTENER_BITLY_PASSWORD', ''),
     ],
+    'bitly-gat'       => [
+        'method' => env('URL_SHORTENER_BITLY_GENERIC_ACCESS_TOKEN', ''),
+    ],
     'connect_timeout' => 2,
     'timeout'         => 2,
 ];
